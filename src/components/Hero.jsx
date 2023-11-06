30a4188ade7052563e7d5b74dddf76cd535a6730
TN <147845928+tndevenv@users.noreply.github.com>
2023-11-06 16:35:47 -0500

    Update logo and color.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ function Hero() {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-        <img src={logo} alt="sumz_logo" className='w-28 object-contain' />
+        <img src={logo} alt="sum_logo" className='w-28 object-contain' />
 
         <button type='button'
           onClick={() => window.open('https://github.com/traycn')}
